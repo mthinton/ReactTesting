bbc756307eb5d18af33fc604c0265e898bced2b9
mthintoncoding <mthinton@gmail.com>
2017-07-07 00:25:12 -0400

    Now successfully tests for image and gallery compenents

--- test/test-gallery.js
+++ test/test-gallery.js
@@ -5,13 +5,30 @@ import chai from 'chai';
 const should = chai.should();
 
 import Gallery from '../js/components/gallery';
+import Image from '../js/components/image'
 
 describe('Gallery component', function(){
 	it('Renders images and their descriptions', function(){
+		const url = "http://www.example.com/image.png";
+		const description = "some things";
+
+
 		
 		const renderer = TestUtils.createRenderer();
-		renderer.render(<Gallery images={[]}/>);
+		renderer.render(<Gallery images={[{url:url, description:description}]}/>);
 		const result = renderer.getRenderOutput();
 		result.props.className.should.equal('gallery');
+
+
+		const gallery = result.props.children[0];
+		gallery.type.should.equal(Image);
+		gallery.props.url.should.equal(url);
+		gallery.props.description.should.equal(description)
+		Object.keys(gallery.props).length.should.equal(2);
+
+		
+
+
+
 	})
 })
